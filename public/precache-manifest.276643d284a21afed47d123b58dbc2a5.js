3cef075d8200c89dfca2022c940b6e0759e80d67
Андрей <you@example.com>
2019-07-10 11:35:22 +0700

    add public

---
+++ public/precache-manifest.276643d284a21afed47d123b58dbc2a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d9351f523581a6a34a971c6286dc4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f4bf6a069612e14f3f1",
+    "url": "/static/css/main.61f093eb.chunk.css"
+  },
+  {
+    "revision": "10c95e8605f18490ee42",
+    "url": "/static/js/2.4a914921.chunk.js"
+  },
+  {
+    "revision": "2f4bf6a069612e14f3f1",
+    "url": "/static/js/main.7a7441bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
